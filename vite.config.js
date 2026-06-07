f32857eb9aab55e1ff47dbf77eac30aed2994a37
XingYuehana <XingYuehana@gmail.com>
2026-06-07 21:14:11 +0800

    修正 base 路徑為 vue-hw5

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/vue-sfc/',
+  base: '/vue-hw5/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
